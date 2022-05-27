507689069e20a8941f7a2f425b1798f333aff3b1
Kieran Ryan <kierankilkenny@gmail.com>
2022-05-27 20:26:43 +0100

    Exception suppression

---
+++ src/bites/exception_suppression.py
@@ -0,0 +1,19 @@
+from contextlib import contextmanager
+
+
+@contextmanager
+def ignore(*exceptions):
+    try:
+        yield
+    except exceptions:
+        pass
+
+
+def _raise_exception():
+    raise TypeError
+
+
+if __name__ == "__main__":
+    with ignore(ValueError, TypeError):
+        _raise_exception()
+    print("Exception was ignored")
