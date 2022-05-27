5592e96aea521c330e248a493f0b93daa24a6e79
Kieran Ryan <kierankilkenny@gmail.com>
2022-05-27 22:28:21 +0100

    Commented iterables map output

--- src/bites/iterables_map.py
+++ src/bites/iterables_map.py
@@ -6,3 +6,11 @@ urlpatterns = [*map(url, ["api", "home", "login"])]
 
 if __name__ == "__main__":
     print(urlpatterns)
+
+    """
+    [
+      'https://site.com/api',
+      'https://site.com/home',
+      'https://site.com/login',
+    ]
+    """
