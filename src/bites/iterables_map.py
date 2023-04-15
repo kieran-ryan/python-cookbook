5aeb233ab70edc99448035bd8d44b7901d2cca5b
Kieran Ryan <kierankilkenny@gmail.com>
2023-04-15 01:58:30 +0100

    Cast to list instead of unpack

--- src/bites/iterables_map.py
+++ src/bites/iterables_map.py
@@ -2,7 +2,7 @@ def url(service):
     return f"https://site.com/{service}"
 
 
-urlpatterns = [*map(url, ["api", "home", "login"])]
+urlpatterns = list(map(url, ["api", "home", "login"]))
 
 if __name__ == "__main__":
     print(urlpatterns)
